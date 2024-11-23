f047ec6a43bf1839c38ae1fe8cddcc2841b2317c
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-11-23 16:07:23 +0100

    sas

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,14 +14,14 @@ function Hero() {
           <h1 className="mb-6 text-4xl sm:text-5xl md:text-6xl font-[800] leading-tight">
             Empowering Africa's <br /> Sustainable Future
           </h1>
-          <p className="mb-6 text-[18px] break-words w-[80%] sm:text-xl">
+          <p className="mb-6 text-[18px] break-words w-[80%] sm:text-xl md:text-lg">
             Driving innovation in renewable energy, <span className="block sm:inline">climate change resilience,</span> and the green economy.
           </p>
-          <div className="space-x-10 ">
-            <button className="px-6 py-3 text-[16px] font-[500] text-white rounded-lg bg-[#49AF45] hover:bg-[#1d8319] hover:outline-none">
+          <div className="space-x-10 sm:space-x-6 sm:mt-4">
+            <button className="px-6 py-3 text-[16px] font-[500] text-white rounded-lg bg-[#49AF45] hover:bg-[#1d8319] hover:outline-none sm:px-4 sm:py-2">
               Get Started
             </button>
-            <button className="px-6 py-3 font-[500] semibold text-white bg-transparent border-2 border-white rounded-lg text- hover:bg-[#49AF45] hover:outline-none text-[16px]">
+            <button className="px-6 py-3 font-[500] semibold text-white bg-transparent border-2 border-white rounded-lg text-[16px] hover:bg-[#49AF45] hover:outline-none sm:px-4 sm:py-2">
               Read More
             </button>
           </div>
